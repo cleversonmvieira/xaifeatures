c1b6afa306e2b61e3cf0dd2e9bad5df359420df1
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-17 17:25:47 -0300

    update equilization

--- equalization.py
+++ equalization.py
@@ -1,9 +1,4 @@
-import numpy as np
-import cv2
-import utils_mod
-from skimage import io, exposure
-import os
-from matplotlib import pyplot as plt
+from skimage import exposure
 
 def equalization(img):
     img_eq = exposure.equalize_adapthist(img, clip_limit=0.05)
