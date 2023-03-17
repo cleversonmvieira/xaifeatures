c036c22097cdf7fe0d4cfd7bb48730844d4be55e
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-17 14:45:29 -0300

    update main

--- roi.py
+++ roi.py
@@ -3,38 +3,5 @@ from  PIL import Image
 
 def isolaNervo(img, rSize):
     img = Image.open(img)
-    altura,largura,cores = img.shape    
-    if altura < largura :        
-        rH = rSize
-        rW = int(rSize*largura/altura)
-        fw = largura/rW
-        fh = altura/rH
-    else:        
-        rW = rSize
-        rH = int(rSize*altura/largura)
-        fw = largura/rW
-        fh = altura/rH
-    img = cv2.resize(img,(rW,rH),interpolation=cv2.INTER_CUBIC)           
     
-    W = largura
-    w = 0
-    H = altura
-    h = 0
-    nerve = int(altura / 5.4)
-
-    if (w > rW or h > rH):
-        w = int(rW//2 - rSize//4)
-        W = int(rW//2 + rSize//4)
-        h = int(rH//2 - rSize//4)
-        H = int(rH//2 + rSize//4)
-    a,l,c = img.shape
-    img = img[h:H,w:W]
-    f = a//2
-    a,l,c = img.shape
-
-    img = cv2.resize(img,(150,150),interpolation=cv2.INTER_CUBIC)
-    an,ln,cn = img.shape
-    fnW = l/ln
-    fnH = a/an
-    
-    return int(W*fw),int(w*fw),int(H*fh),int(h*fh),fw,fh,nerve,fnW,fnH,img
+    return img
