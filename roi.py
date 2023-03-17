3df5a513a35ef704c6b769a2d333085d468e8e10
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-17 14:44:26 -0300

    update roi

--- roi.py
+++ roi.py
@@ -1,6 +1,8 @@
 import cv2
+from  PIL import Image
 
 def isolaNervo(img, rSize):
+    img = Image.open(img)
     altura,largura,cores = img.shape    
     if altura < largura :        
         rH = rSize
