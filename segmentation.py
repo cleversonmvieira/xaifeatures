a0a4486d0ac33146a97a4f78c7970c298c918282
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-17 17:36:39 -0300

    update segmentation

--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,9 @@
 import cv2
+import numpy as np
 
 def segmentation_disc(isolated_nerve, img_eq):
+    iq = np.float32(img_eq)
+    
     orig = isolated_nerve.copy()
     orig_2 = img_eq.copy()
     
