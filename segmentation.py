49751acd31a544026c4b6284f41f43b67273dacf
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 13:44:56 -0300

    update segmentation

--- segmentation.py
+++ segmentation.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 
 def segmentation_disc(isolated_nerve, img_eq):
+    img_eq_float = np.float32(img_eq)
     orig = isolated_nerve.copy()
-    orig_2 = img_eq.copy()
+    orig_2 = img_eq_float.copy()
     
-    gray = cv2.cvtColor(img_eq, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img_eq_float, cv2.COLOR_BGR2GRAY)
 
     gray = np.float32(gray)
 
