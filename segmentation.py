bfe0ef9312fe74dbc46bbba42f9147b88cb8cf06
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 14:17:50 -0300

    update seg

--- segmentation.py
+++ segmentation.py
@@ -7,7 +7,7 @@ def segmentation_disc(isolated_nerve, img_eq):
     img_eq = cv2.convertScaleAbs(img_eq)
     #gray = color.rgb2gray(img_eq)
     #gray = util.img_as_uint(gray)
-    norm = ppool.zeros((800,800))
+    norm = ppool.zeros((15,15))
     final = cv2.normalize(img_eq,  norm, 0, 255, cv2.NORM_MINMAX)
     gray = cv2.cvtColor(img_eq, cv2.COLOR_RGB2GRAY)
 
