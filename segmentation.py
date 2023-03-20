f4335bd9f4d61d43dd6cce72748df8f0684bff32
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 13:56:34 -0300

    update segmentation

--- segmentation.py
+++ segmentation.py
@@ -1,20 +1,19 @@
 import cv2
 import numpy as np
+from skimage import color, util
 
 def segmentation_disc(isolated_nerve, img_eq):
     img_eq = np.float32(img_eq)
-    img_eq = img_eq.convertTo(img_eq,cv2.CV_64F)
-    orig = isolated_nerve.copy()
-    orig_2 = img_eq.copy()
+    gray = color.rgb2gray(img_eq)
     
-    gray = cv2.cvtColor(img_eq, cv2.COLOR_RGB2GRAY)
+    #gray = cv2.cvtColor(img_eq, cv2.COLOR_RGB2GRAY)
 
     #gray = np.float32(gray)
 
-    equ = cv2.equalizeHist(gray)
+    #equ = cv2.equalizeHist(gray)
 
-    median = cv2.medianBlur(equ,15)
+    #median = cv2.medianBlur(equ,15)
 
-    _, disc = cv2.threshold(gray, 195, 255, cv2.THRESH_BINARY)
+    #_, disc = cv2.threshold(gray, 195, 255, cv2.THRESH_BINARY)
     
     return gray
