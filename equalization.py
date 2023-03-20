9da156fed08e91526c5f9131e7d83c991b02a891
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 14:13:11 -0300

    update equalization

--- equalization.py
+++ equalization.py
@@ -2,5 +2,5 @@ from skimage import exposure
 import numpy as np
 
 def equalization(img):
-    img_eq = exposure.equalize_adapthist(img, clip_limit=0.05)
+    img_eq = exposure.equalize_adapthist(img, clip_limit=1)
     return img_eq
