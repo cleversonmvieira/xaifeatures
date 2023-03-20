c57feaf146fe3104fb236239ed3837870627988b
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 13:19:50 -0300

    update equalization

--- equalization.py
+++ equalization.py
@@ -1,6 +1,7 @@
 from skimage import exposure
+import cv2
 
 def equalization(img):
     img_eq = exposure.equalize_adapthist(img, clip_limit=0.05)
-
+    img_eq= cv2.imdecode(img_eq, 1)
     return img_eq
