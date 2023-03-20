0d9056d00d5c95d909b08979fb4ba50866b46dce
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2023-03-20 14:02:19 -0300

    update seg

--- segmentation.py
+++ segmentation.py
@@ -3,7 +3,7 @@ import numpy as np
 from skimage import color, util
 
 def segmentation_disc(isolated_nerve, img_eq):
-    img_eq = np.float32(img_eq)
+    img_eq = np.uint8(img_eq)
     gray = color.rgb2gray(img_eq)
     #gray = util.img_as_uint(gray)
     
@@ -15,6 +15,6 @@ def segmentation_disc(isolated_nerve, img_eq):
 
     #median = cv2.medianBlur(gray,15)
 
-    _, disc = cv2.threshold(gray, 33, 255, cv2.THRESH_BINARY)
+    #_, disc = cv2.threshold(gray, 33, 255, cv2.THRESH_BINARY)
     
-    return disc
+    return gray
